2c3c4626ce19bd0c1f8aac435a570784e354a713
ruben <ruben.kabanga-muya@epitech.eu>
2022-10-13 14:59:11 +0200

    step 2 done

--- colle.php
+++ colle.php
@@ -1,8 +1,14 @@
 <?php
 
-function drawRepeated($draw, $count){
+function drawRepeated($draw, $x, $y = NULL, $coords = NULL){
     $result = "";
-    for ($i = 0; $i < $count; $i++) {
+    for ($i = 0; $i < $x; $i++) {
+        if ($coords){
+            $draw = "|   |";
+            if (in_array([$i, $y], $coords))
+                $draw = "| X |";
+        }
+
         if (substr($result, -1) == substr($draw, 0, 1)) {
             $result .= substr($draw, 1);
         }
@@ -13,15 +19,17 @@ function drawRepeated($draw, $count){
     return $result;
 }
 
-function colle(int $x, int $y) : void {
+function colle(int $x, int $y, array $coords) : void {
+    define("COORDS", $coords);
     if ($x > 0 & $y > 0) {
         for ($i = 0; $i < $y; $i++) {
             echo drawRepeated("+–––+", $x) . PHP_EOL;
-            echo drawRepeated("|   |", $x) . PHP_EOL;
+            echo drawRepeated("|   |", $x, $i, $coords) . PHP_EOL;
         }
         echo drawRepeated("+–––+", $x) . PHP_EOL;
 
     }
 }
 
-//colle(1,2);
+colle(4,5, [ [0,0], [0, 1], [0,4]]);
+//var_dump(COORDS);
